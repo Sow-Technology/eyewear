6c7b7b838e9da93a7954682d9aa1e7f182181d1a
Sajag Agrawal <sajagaga2806@gmail.com>
2024-06-06 22:20:16 +0530

    backend_files added

---
+++ eyewear/accounts/views.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework import status
+
+@api_view(['POST'])
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        
+        if not username or not email or not password:
+            return Response({'error': 'Username, email, and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'Username is already taken.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if User.objects.filter(email=email).exists():
+            return Response({'error': 'Email is already registered.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            return Response({'message': 'Registration successful.'}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(['POST'])
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        email = request.data.get('email')
+        password = request.data.get('password')
+        
+        if not email or not password:
+            return Response({'error': 'Email and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            return Response({'message': 'Login successful.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials. Please try again.'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
